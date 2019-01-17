5aadef4d1c16d612a6509178196fbc3b963d8c69
wikkoz <wikkoz@users.noreply.github.com>
2019-01-17 08:42:11 +0100

    Initializing airlines for all brokers

--- src/main/scala/com/DependencyWiring.scala
+++ src/main/scala/com/DependencyWiring.scala
@@ -7,17 +7,17 @@ import scala.concurrent.ExecutionContext
 
 trait DependencyWiring {
 
-  private def initAirlineBrokersActors(n: Int): Map[Int, ActorRef] = {
+  private def initAirlineBrokersActors(): Map[Int, ActorRef] = {
     Array.range(0, config.brokers)
       .map(i => Tuple2(i, getAirlineBroker(i)))
       .toMap
   }
 
   private def getAirlineBroker(i: Int): ActorRef = {
-    system.actorOf(AirlineBrokerActor.props(getAirlinesActors(i), config.brokersThreads))
+    system.actorOf(AirlineBrokerActor.props(airlines, config.brokersThreads))
   }
 
-  private def getAirlinesActors(i: Int): Map[Int, ActorRef] = {
+  private def initAirlineActors(): Map[Int, ActorRef] = {
     Array.range(0, config.airlines)
       .map(airline => Tuple2(airline, system.actorOf(AirlineActor.props(airline))))
       .toMap
@@ -27,5 +27,6 @@ trait DependencyWiring {
   def config: Config
   def executionContext: ExecutionContext
 
-  final val airlineBrokers: Map[Int, ActorRef] = initAirlineBrokersActors(4)
+  final val airlineBrokers: Map[Int, ActorRef] = initAirlineBrokersActors()
+  final val airlines: Map[Int, ActorRef] = initAirlineActors()
 }
